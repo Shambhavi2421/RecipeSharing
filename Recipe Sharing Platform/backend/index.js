2ec446acf70fd6b7a996676f91f07960ecd1c2e4
shambhavi2421 <shambhaviekbote7@gmail.com>
2025-03-08 19:50:17 +0530

    Fixed module issue

--- Recipe Sharing Platform/backend/index.js
+++ Recipe Sharing Platform/backend/index.js
@@ -1,13 +1,16 @@
+import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import { userRouter } from "./routes/user.js";
 import { recipesRouter } from "./routes/recipe.js";
+require("dotenv").config();
 
-const express = require("express");
+// const express = require("express");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use("/auth", userRouter);
 app.use("/recipe", recipesRouter);
